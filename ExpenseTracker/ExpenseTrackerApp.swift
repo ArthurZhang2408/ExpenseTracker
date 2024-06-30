f713966a319baa779f4c588f3ea7742ceebbc956
Arthur Zhang <arthurzhang2408@hotmail.com>
2024-06-29 20:48:48 -0400

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Arthur Zhang on 2024-06-29.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
