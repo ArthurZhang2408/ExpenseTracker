4e99897e44e6b0c0f7bb5a50a5f221468e8506c9
Arthur Zhang <arthurzhang2408@hotmail.com>
2024-10-01 00:21:01 +0800

    deleting API LEAK

--- ExpenseTracker/Views/ItemView.swift
+++ ExpenseTracker/Views/ItemView.swift
@@ -8,8 +8,50 @@
 import SwiftUI
 
 struct ItemView: View {
+    var cObj: ExpenseCategory = ExpenseCategory(id: "Grocery", img: "🥬", spent: 400000, subCategories: [])
+    var clicked: Bool = false
+//    BudgetModel(dict: [ "name": "Auto & Transport",
+//                                                       "icon": "auto_&_transport",
+//                                                       "spend_amount": 25.99,
+//                                                       "total_amount": 400,
+//                                                       "color": Color.secondaryG ] )
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack{
+            HStack{
+                
+                Text(cObj.img)
+                    .font(.customfont(.semibold, fontSize: 25))
+                    .frame(width: 35, height: 20)
+                    .foregroundColor(.white)
+                
+                VStack{
+                    Text(cObj.id)
+                        .font(.customfont(.semibold, fontSize: 14))
+                        .foregroundColor(.white)
+                        .frame(minWidth: 0, maxWidth: 100, alignment: .leading)
+                }
+                
+                VStack(alignment: .trailing){
+                    Text("This Month Spent $\(cObj.spent.formatted())")
+                        .font(.customfont(.semibold, fontSize: 14))
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity, alignment: .trailing)
+                    
+                }
+            }
+//            ProgressView(value: bObj.perSpend, total: 1)
+//                .tint(bObj.color)
+        }
+        
+        .padding(15)
+        .frame(minWidth: 0, maxWidth: .infinity)
+        .background(Color.gray60.opacity( 0.2  ))
+        .overlay {
+            RoundedRectangle(cornerRadius:  12)
+                .stroke(  Color.gray70, lineWidth: 1)
+        }
+        .cornerRadius(12)
     }
 }
 
